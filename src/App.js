0c2209579a393d336b0e5ecc9b524f2eb25d628d
Roy Manik <roymanlanjutmanik@gmail.com>
2022-08-13 01:08:43 +0700

    init push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import ReactMapboxGl from "react-mapbox-gl";
+import DrawControl from "react-mapbox-gl-draw";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+
+const Map = new ReactMapboxGl({
+  accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+});
 
 function App() {
+
+  const onDrawCreate = ({ features }) => {
+    console.log(features);
+  };
+
+  const onDrawUpdate = ({ features }) => {
+    console.log(features);
+  };
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="sidebar">
+        Longitude: "lng" | Latitude: "lat" | Zoom: "zoom"
+      </div>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9" // eslint-disable-line
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <DrawControl/>
+      </Map>
+      <div className="calculation-box">
+        <p>Click the map to draw a polygon.</p>
+        <div id="calculated-area"></div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import ReactMapboxGl from "react-mapbox-gl";
+import DrawControl from "react-mapbox-gl-draw";
+import * as turf from "@turf/turf";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiZmFrZXVzZXJnaXRodWIiLCJhIjoiY2pwOGlneGI4MDNnaDN1c2J0eW5zb2ZiNyJ9.mALv0tCpbYUPtzT7YysA2g",
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [geoData, setGeoData] = useState([]);
+
+  const onDrawCreate = ({ features }) => {
+    console.log("feature:", features);
+    setGeoData((oldValue) => [...oldValue, features[0]]);
+  };
+
+  const onDrawUpdate = ({ features }) => {
+    Object.values(features).map((feature) => {
+      setGeoData((oldValue) =>
+        oldValue.map((e) => (e.id === feature.id ? feature : e))
+      );
+    });
+  };
+
+  const onDelete = ({ features }) => {
+    Object.values(features).map((feature) => {
+      setGeoData((oldValue) => oldValue.filter((e) => e.id !== feature.id));
+    });
+  };
+
+  useEffect(() => {
+    console.log(geoData.length);
+    console.log(geoData);
+  }, [geoData]);
+
+  return (
+    <div>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9" // eslint-disable-line
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <DrawControl
+          displayControlsDefault={false}
+          controls={{ polygon: true, line_string: true, trash: true }}
+          onDrawCreate={onDrawCreate}
+          onDrawUpdate={onDrawUpdate}
+          onDrawDelete={onDelete}
+        />
+      </Map>
+      <div className="calculation-box">
+        <p>Click the map to draw a shape.</p>
+        <div id="calculated-area"></div>
+      </div>
+      <div style={{ border: "1px solid black",marginTop: "1rem" }} >
+        {geoData &&
+          geoData.map((item) => <p key={item.id}>{JSON.stringify(item)}</p>)}
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
